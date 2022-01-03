fbf138623ee5f377bdc0f396a3f93734f7a72546
YUSUF BADAT <y.m.badat@edu.salford.ac.uk>
2022-01-03 16:06:46 +0000

    CSS PURGE

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
   return src('assets/sass/**/*.scss')//sourcfile
     .pipe(sass())//compiler
+    .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('assets/css'))//output to css in root Dir
 }
 
 function watchTask() {
-  watch(['assets/sass/**/*.scss'], buildStyles)///Watches Source Sass File
+  watch(['assets/sass/**/*.scss', '*.html'], buildStyles)///Watches Source Sass File
 }
 
 exports.default = series(buildStyles, watchTask)//Export Files
